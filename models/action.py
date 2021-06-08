fee6751d186d9d22bfe4a46a02ce2c05a7f03e7a
Jon Clayton <supertent@gmail.com>
2021-06-08 13:09:19 +0100

    Jmc fortigate get configs (#1)
    * fix for timeout argument being unexpected
    
    * various changes to allow getting configs and saving locally to allow for further batfish operation2
    
    * robust writing to file - tested thoroughly
    
    * mods to fortigate download configs  allow working with gitops (passing the dest folder in eventData
    
    * manifest changes
    
    Co-authored-by: root <root@lm2.wizznet.co.uk>
    Co-authored-by: z1pti3 <66521110+z1pti3@users.noreply.github.com>
    Co-authored-by: Jon Clayton <m87tech@gmail.com>

--- models/action.py
+++ models/action.py
@@ -4,6 +4,7 @@ from plugins.remote.includes import linux, windows, fortigate
 
 import jimi
 
+
 class _remoteConnectLinux(action._action):
     host = str()
     user = str()
@@ -11,69 +12,89 @@ class _remoteConnectLinux(action._action):
     password = str()
     isPortForward = bool()
     port_forward = str()
-    
-    def doAction(self,data):
-        host    = helpers.evalString(self.host,{"data" : data["flowData"]})
-        user    = helpers.evalString(self.user,{"data" : data["flowData"]})
-        port    = helpers.evalString(self.port_forward,{"data" : data["flowData"]})
+
+    def doAction(self, data):
+        host = helpers.evalString(self.host, {"data": data["flowData"]})
+        user = helpers.evalString(self.user, {"data": data["flowData"]})
+        port = helpers.evalString(self.port_forward, {"data": data["flowData"]})
         if self.password.startswith("ENC"):
             password = auth.getPasswordFromENC(self.password)
         else:
             password = ""
-        keyfile = helpers.evalString(self.keyfile,{"data" : data["flowData"]})
+        keyfile = helpers.evalString(self.keyfile, {"data": data["flowData"]})
 
         if self.isPortForward:
             if keyfile != "":
                 if password:
-                    client= linux.linux(host,user,keyFile=keyfile,password=password,port_forward=True,port=port)
+                    client = linux.linux(
+                        host,
+                        user,
+                        keyFile=keyfile,
+                        password=password,
+                        port_forward=True,
+                        port=port,
+                    )
                 else:
-                    client = linux.linux(host,user,keyFile=keyfile,port_forward=True,port=port)
+                    client = linux.linux(
+                        host, user, keyFile=keyfile, port_forward=True, port=port
+                    )
             else:
-                client = linux.linux(host,user,password=password)
+                client = linux.linux(host, user, password=password)
         else:
             if keyfile != "":
                 if password != "":
-                    client = linux.linux(host,user,keyFile=keyfile,password=password)
+                    client = linux.linux(host, user, keyFile=keyfile, password=password)
                 else:
-                    client = linux.linux(host,user,keyFile=keyfile)
+                    client = linux.linux(host, user, keyFile=keyfile)
             else:
-                client = linux.linux(host,user,password=password)
+                client = linux.linux(host, user, password=password)
 
         if client.client != None:
-            data["eventData"]["remote"]={}
+            data["eventData"]["remote"] = {}
             data["eventData"]["remote"]["client"] = client
 
             if self.isPortForward:
                 data["eventData"]["remote"]["port"] = client.tunnelPort
-            return {"result" : True, "rc" : 0, "msg" : "Connection successful"}
+            return {"result": True, "rc": 0, "msg": "Connection successful"}
         else:
-            return {"result" : False, "rc" : 403, "msg" : "Connection failed - {0}".format(client.error)}
+            return {
+                "result": False,
+                "rc": 403,
+                "msg": "Connection failed - {0}".format(client.error),
+            }
 
-    def setAttribute(self,attr,value,sessionData=None):
+    def setAttribute(self, attr, value, sessionData=None):
         if attr == "password" and not value.startswith("ENC "):
             self.password = "ENC {0}".format(auth.getENCFromPassword(value))
             return True
-        return super(_remoteConnectLinux, self).setAttribute(attr,value,sessionData=sessionData)
+        return super(_remoteConnectLinux, self).setAttribute(
+            attr, value, sessionData=sessionData
+        )
+
 
 class _remoteLinuxStartPortForward(action._action):
-
-    def doAction(self,data):
+    def doAction(self, data):
         try:
             client = data["eventData"]["remote"]["client"]
         except KeyError:
             client = None
 
-        if client:                
-            test,port = client.start_port_forward()
+        if client:
+            test, port = client.start_port_forward()
             data["eventData"]["remote"]["port"] = port
             data["eventData"]["remote"]["portForwardStatus"] = True
-            return {"result" : True, "rc" : 0, "msg" : "Port forwarding started", "portForwardStatus" : True}
+            return {
+                "result": True,
+                "rc": 0,
+                "msg": "Port forwarding started",
+                "portForwardStatus": True,
+            }
         else:
-            return {"result" : False, "rc" : 403, "msg" : "No connection found"}
+            return {"result": False, "rc": 403, "msg": "No connection found"}
+
 
 class _remoteLinuxStopPortForward(action._action):
-
-    def doAction(self,data):
+    def doAction(self, data):
         try:
             client = data["eventData"]["remote"]["client"]
             port = data["eventData"]["remote"]["port"]
@@ -81,35 +102,43 @@ class _remoteLinuxStopPortForward(action._action):
             client = None
             port = None
 
-        if client and port:    
+        if client and port:
             client.stop_port_forward(client)
             data["eventData"]["remote"]["portForwardStatus"] = False
-            return {"result" : True, "rc" : 0, "msg" : "Port forwarding stopped"}
+            return {"result": True, "rc": 0, "msg": "Port forwarding stopped"}
         else:
-            return {"result" : False, "rc" : 403, "msg" : "No connection found"}
+            return {"result": False, "rc": 403, "msg": "No connection found"}
+
 
 class _remoteConnectWindows(action._action):
     host = str()
     user = str()
     password = str()
 
-    def doAction(self,data):
-        host = helpers.evalString(self.host,{"data" : data["flowData"]})
-        user = helpers.evalString(self.user,{"data" : data["flowData"]})
+    def doAction(self, data):
+        host = helpers.evalString(self.host, {"data": data["flowData"]})
+        user = helpers.evalString(self.user, {"data": data["flowData"]})
         password = auth.getPasswordFromENC(self.password)
 
-        client = windows.windows(host,user,password)
+        client = windows.windows(host, user, password)
         if client.client != None:
-            data["eventData"]["remote"]={"client" : client}
-            return {"result" : True, "rc" : 0, "msg" : "Connection successful"}
+            data["eventData"]["remote"] = {"client": client}
+            return {"result": True, "rc": 0, "msg": "Connection successful"}
         else:
-            return {"result" : False, "rc" : 403, "msg" : "Connection failed - {0}".format(client.error)}
+            return {
+                "result": False,
+                "rc": 403,
+                "msg": "Connection failed - {0}".format(client.error),
+            }
 
-    def setAttribute(self,attr,value,sessionData=None):
+    def setAttribute(self, attr, value, sessionData=None):
         if attr == "password" and not value.startswith("ENC "):
             self.password = "ENC {0}".format(auth.getENCFromPassword(value))
             return True
-        return super(_remoteConnectWindows, self).setAttribute(attr,value,sessionData=sessionData)
+        return super(_remoteConnectWindows, self).setAttribute(
+            attr, value, sessionData=sessionData
+        )
+
 
 class _remoteConnectFortigate(action._action):
     host = str()
@@ -119,39 +148,56 @@ class _remoteConnectFortigate(action._action):
     password = str()
     maxRecvTime = 5
 
-    def doAction(self,data):
-        host = helpers.evalString(self.host,{"data" : data["flowData"]})
-        port = helpers.evalString(self.port,{"data" : data["flowData"]})
-        deviceHostname = helpers.evalString(self.deviceHostname,{"data" : data["flowData"]})
-        user = helpers.evalString(self.user,{"data" : data["flowData"]})
+    def doAction(self, data):
+        host = helpers.evalString(self.host, {"data": data["flowData"]})
+        port = helpers.evalString(self.port, {"data": data["flowData"]})
+        deviceHostname = helpers.evalString(
+            self.deviceHostname, {"data": data["flowData"]}
+        )
+        user = helpers.evalString(self.user, {"data": data["flowData"]})
         if self.password.startswith("ENC"):
             password = auth.getPasswordFromENC(self.password)
         else:
             password = ""
 
-        client = fortigate.fortigate(host,deviceHostname,user,password=password,port=port,maxRecvTime=self.maxRecvTime)
+        client = fortigate.fortigate(
+            host,
+            deviceHostname,
+            user,
+            password=password,
+            port=port,
+            maxRecvTime=self.maxRecvTime,
+        )
 
         if client.client != None:
-            data["eventData"]["remote"]={"client" : client}
-            return {"result" : True, "rc" : 0, "msg" : "Connection successful"}
+            data["eventData"]["remote"] = {"client": client}
+            return {"result": True, "rc": 0, "msg": "Connection successful"}
         else:
-            return {"result" : False, "rc" : 403, "msg" : "Connection failed - {0}".format(client.error)}
+            return {
+                "result": False,
+                "rc": 403,
+                "msg": "Connection failed - {0}".format(client.error),
+            }
 
-    def setAttribute(self,attr,value,sessionData=None):
+    def setAttribute(self, attr, value, sessionData=None):
         if attr == "password" and not value.startswith("ENC "):
             self.password = "ENC {0}".format(auth.getENCFromPassword(value))
             return True
-        return super(_remoteConnectFortigate, self).setAttribute(attr,value,sessionData=sessionData)
+        return super(_remoteConnectFortigate, self).setAttribute(
+            attr, value, sessionData=sessionData
+        )
+
 
 class _remoteDisconnect(action._action):
-    def doAction(self,data):
+    def doAction(self, data):
         try:
             client = data["eventData"]["remote"]["client"]
         except KeyError:
             client = None
         if client:
             client.disconnect()
-        return {"result" : True, "rc" : 0, "msg" : "Connection disconnected"}
+        return {"result": True, "rc": 0, "msg": "Connection disconnected"}
+
 
 class _remoteCommand(action._action):
     command = str()
@@ -159,26 +205,92 @@ class _remoteCommand(action._action):
     runAs = str()
     timeout = 300
 
-    def doAction(self,data):
-        command = helpers.evalString(self.command,{"data" : data["flowData"]})
+    def doAction(self, data):
+        command = helpers.evalString(self.command, {"data": data["flowData"]})
         try:
             client = data["eventData"]["remote"]["client"]
         except KeyError:
             client = None
         if client:
-            exitCode, output, errors = client.command(command,elevate=self.elevate,runAs=self.runAs,timeout=self.timeout)
-            
+            exitCode, output, errors = client.command(
+                command, elevate=self.elevate, runAs=self.runAs, timeout=self.timeout
+            )
+
             if exitCode != None:
-                return {"result" : True, "rc" : exitCode, "msg" : "Command succesfull", "data" : output, "errors" : errors}
+                return {
+                    "result": True,
+                    "rc": exitCode,
+                    "msg": "Command succesfull",
+                    "data": output,
+                    "errors": errors,
+                }
             else:
-                return {"result" : False, "rc" : 255, "msg" : client.error, "data" : "", "errors" : ""}
+                return {
+                    "result": False,
+                    "rc": 255,
+                    "msg": client.error,
+                    "data": "",
+                    "errors": "",
+                }
         else:
-            return {"result" : False, "rc" : 403, "msg" : "No connection found"}
+            return {"result": False, "rc": 403, "msg": "No connection found"}
+
+
+class _remoteDownloadConfig(action._action):
+    command = str()
+    elevate = bool()
+    runAs = str()
+    timeout = 60
+    dstFolder = str()
+    deviceModel = str()
+
+    def doAction(self, data):
+
+        # Add dest folder to eventData
+        data["eventData"]["backup_args"] = {}
+        data["eventData"]["backup_args"] = {"dst_folder": self.dstFolder}
+
+        command = helpers.evalString(self.command, {"data": data["flowData"]})
+        try:
+            client = data["eventData"]["remote"]["client"]
+        except KeyError:
+            client = None
+        # set dest folder sane defaults
+        if self.dstFolder == None or self.dstFolder == "":
+            self.dstFolder = "/shared/data/storage/firewall-configs/snapshots/configs"
+        if client:
+            exitCode, errors = client.download_config(
+                command,
+                elevate=self.elevate,
+                runAs=self.runAs,
+                timeout=self.timeout,
+                dstFolder=self.dstFolder,
+            )
+
+            if exitCode != None:
+                return {
+                    "result": True,
+                    "rc": exitCode,
+                    "msg": "Command succesfull",
+                    "data": "config saved!",
+                    "errors": errors,
+                }
+            else:
+                return {
+                    "result": False,
+                    "rc": 255,
+                    "msg": client.error,
+                    "data": "",
+                    "errors": "",
+                }
+        else:
+            return {"result": False, "rc": 403, "msg": "No connection found"}
+
 
 class _remoteReboot(action._action):
     timeout = int()
 
-    def doAction(self,data):
+    def doAction(self, data):
         try:
             client = data["eventData"]["remote"]["client"]
         except KeyError:
@@ -189,11 +301,12 @@ class _remoteReboot(action._action):
                 timeout = self.timeout
             result = client.reboot(timeout)
             if result:
-                return {"result" : True, "rc" : 0, "msg" : "Reboot successful"}
+                return {"result": True, "rc": 0, "msg": "Reboot successful"}
             else:
-                return {"result" : False, "rc" : 255, "msg" : "Reboot failed"}
+                return {"result": False, "rc": 255, "msg": "Reboot failed"}
         else:
-            return {"result" : False, "rc" : 403, "msg" : "No connection found"}
+            return {"result": False, "rc": 403, "msg": "No connection found"}
+
 
 class _remoteDownload(action._action):
     remoteFile = str()
@@ -201,16 +314,22 @@ class _remoteDownload(action._action):
     createMissingFolders = bool()
     useStorage = bool()
 
-    def doAction(self,data):
-        remoteFile = helpers.evalString(self.remoteFile,{"data" : data["flowData"]})
-        localFile = helpers.evalString(self.localFile,{"data" : data["flowData"]})
+    def doAction(self, data):
+        remoteFile = helpers.evalString(self.remoteFile, {"data": data["flowData"]})
+        localFile = helpers.evalString(self.localFile, {"data": data["flowData"]})
 
         if self.useStorage:
             try:
-                localFileClass = jimi.storage._storage().getAsClass(query={ "fileData" : localFile, "systemStorage" : True, "source" : "remoteDownload" })[0]
+                localFileClass = jimi.storage._storage().getAsClass(
+                    query={
+                        "fileData": localFile,
+                        "systemStorage": True,
+                        "source": "remoteDownload",
+                    }
+                )[0]
             except:
                 localFileClass = jimi.storage._storage()
-                localFileClass.new(self.acl,"remoteDownload",localFile,True)
+                localFileClass.new(self.acl, "remoteDownload", localFile, True)
             localFile = localFileClass.getFullFilePath()
 
         client = None
@@ -218,37 +337,64 @@ class _remoteDownload(action._action):
             if "client" in data["eventData"]["remote"]:
                 client = data["eventData"]["remote"]["client"]
         if client:
-            if client.download(remoteFile,localFile,self.createMissingFolders):
+            if client.download(remoteFile, localFile, self.createMissingFolders):
                 if self.useStorage:
                     localFileClass.calculateHash()
-                    return {"result" : True, "rc" : 0, "msg" : "File transfered successful", "storage" : { "fileHash" : localFileClass.fileHash, "_id" : localFileClass._id } }
-                return {"result" : True, "rc" : 0, "msg" : "File transfered successful"}
+                    return {
+                        "result": True,
+                        "rc": 0,
+                        "msg": "File transfered successful",
+                        "storage": {
+                            "fileHash": localFileClass.fileHash,
+                            "_id": localFileClass._id,
+                        },
+                    }
+                return {"result": True, "rc": 0, "msg": "File transfered successful"}
 
-        return {"result" : False, "rc" : 403, "msg" : "File transfer failed - {0}".format(client.error)}
+        return {
+            "result": False,
+            "rc": 403,
+            "msg": "File transfer failed - {0}".format(client.error),
+        }
+
 
 class _remoteUpload(action._action):
     remoteFile = str()
     localFile = str()
     useStorage = bool()
 
-    def doAction(self,data):
-        remoteFile = helpers.evalString(self.remoteFile,{"data" : data["flowData"]})
-        localFile = helpers.evalString(self.localFile,{"data" : data["flowData"]})
+    def doAction(self, data):
+        remoteFile = helpers.evalString(self.remoteFile, {"data": data["flowData"]})
+        localFile = helpers.evalString(self.localFile, {"data": data["flowData"]})
 
         if self.useStorage:
             try:
-                localFilePath = jimi.storage._storage().getAsClass(id=localFile)[0].getLocalFilePath()
+                localFilePath = (
+                    jimi.storage._storage()
+                    .getAsClass(id=localFile)[0]
+                    .getLocalFilePath()
+                )
                 if not localFilePath:
                     raise
                 localFile = localFilePath
             except:
-                return {"result" : False, "rc" : 404, "msg" : "Local file not found within storage store. storageID={0}".format(localFile)}
+                return {
+                    "result": False,
+                    "rc": 404,
+                    "msg": "Local file not found within storage store. storageID={0}".format(
+                        localFile
+                    ),
+                }
 
         try:
             client = data["eventData"]["remote"]["client"]
         except KeyError:
             client = None
         if client:
-            if client.upload(localFile,remoteFile):
-                return {"result" : True, "rc" : 0, "msg" : "File transfered successful"}
-        return {"result" : False, "rc" : 403, "msg" : "File transfer failed - {0}".format(client.error)}
+            if client.upload(localFile, remoteFile):
+                return {"result": True, "rc": 0, "msg": "File transfered successful"}
+        return {
+            "result": False,
+            "rc": 403,
+            "msg": "File transfer failed - {0}".format(client.error),
+        }
